0a6c49e27023eee549dcb12a4e27cf5265276809
Ahmad Tabash <ahmad2tabash@gmail.com>
2025-03-04 01:14:16 +0300

    Project 4 - DynamicArray  Extension 1 to 14

--- Course 13/MainDynamicArray.cpp
+++ Course 13/MainDynamicArray.cpp
@@ -1,26 +1,104 @@
-#include <iostream>
-#include "clsDynamicArray.h"
+//#include <iostream>
+//#include "clsDynamicArray.h"
+//
+//using namespace std;
+//
+//int main()
+//{
+    //clsDynamicArray<int> MyDynamicArray(5);
+    //
+    //MyDynamicArray.SetItem(0, 10);
+    //MyDynamicArray.SetItem(1, 20);
+    //MyDynamicArray.SetItem(2, 30);
+    //MyDynamicArray.SetItem(3, 40);
+    //MyDynamicArray.SetItem(4, 50);
+    //
+    //cout << "\nIs Empty ? "<< MyDynamicArray.IsEmpty() << endl;
+    //cout << "\Array Size : "<< MyDynamicArray.Size() << endl;
+    //
+    //cout << "\Array Items :\n ";
+    //MyDynamicArray.PrintItems();
 
-using namespace std;
+    //MyDynamicArray.Resize(2);
+    //cout << "\Array Size : " << MyDynamicArray.Size() << endl;
+    //cout << "\Array Items after resize 2 :\n ";
+    //MyDynamicArray.PrintItems();
 
-int main()
-{
-    clsDynamicArray<int> MyDynamicArray(5);
 
-    MyDynamicArray.SetItem(0, 10);
-    MyDynamicArray.SetItem(1, 20);
-    MyDynamicArray.SetItem(2, 30);
-    MyDynamicArray.SetItem(3, 40);
-    MyDynamicArray.SetItem(4, 50);
-
-    cout << "\nIs Empty ? "<< MyDynamicArray.IsEmpty() << endl;
-    cout << "\Array Size : "<< MyDynamicArray.Size() << endl;
+    //MyDynamicArray.Resize(10);
+    //cout << "\Array Size : " << MyDynamicArray.Size() << endl;
+    //cout << "\Array Items after resize 10 :\n ";
+    //MyDynamicArray.PrintItems();
     
-    cout << "\Array Items :\n ";
-    
-    MyDynamicArray.PrintItems();
 
-    system("pause>0");
-    
-    return 0;
-}
+    //cout << "\nItem(2) : " << MyDynamicArray.GetItem(2) << endl;
+
+
+    //MyDynamicArray.Reverse();
+    //cout << "\Array Items after reverse :\n ";
+    //MyDynamicArray.PrintItems();
+
+
+    //MyDynamicArray.Clear();
+    //cout << "\Array Items after Clear :\n ";
+    //MyDynamicArray.PrintItems();
+
+    //MyDynamicArray.DeleteItemByIndex(2);
+    //cout << "\Array Items after deleting item(2) :\n ";
+    //MyDynamicArray.PrintItems();
+   
+    //MyDynamicArray.DeleteFirstItem();
+    //cout << "\Array Items after deleting FirstItem :\n ";
+    //MyDynamicArray.PrintItems();
+    //cout << "\Array Size : " << MyDynamicArray.Size() << endl;
+    //
+    //MyDynamicArray.DeleteLastItem();
+    //cout << "\Array Items after deleting Last Item :\n ";
+    //MyDynamicArray.PrintItems();
+    //cout << "\Array Size : " << MyDynamicArray.Size() << endl;
+
+
+    //int Index = MyDynamicArray.Find(10);
+    //if (Index == -1)
+    //    cout << "\nItem was not Found :-(\n ";
+    //else
+    //    cout << "\n10 is found at index : " << Index;
+
+    //MyDynamicArray.DeleteItem(10);
+    //cout << "\n\nArray Items after deleting 10:";
+    //cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    //MyDynamicArray.PrintItems();   
+  
+
+    //MyDynamicArray.InsertAt(2,500);
+    //cout << "\n\nArray Items after Insert 500 at index 2 :";
+    //cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    //MyDynamicArray.PrintItems();
+
+    //MyDynamicArray.InsertAtBeginning(400);
+    //cout << "\n\nArray after insert 400 at Begining:";
+    //cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    //MyDynamicArray.PrintItems();
+    //
+    //MyDynamicArray.InsertBefore(2, 500);
+    //cout << "\n\nArray after insert 500 before index 2:";
+    //cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    //MyDynamicArray.PrintItems();
+    //
+    //MyDynamicArray.InsertAfter(7, 600);
+    //cout << "\n\nArray after insert 600 after index 2:";
+    //cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    //MyDynamicArray.PrintItems();
+    //
+    //
+    //MyDynamicArray.InsertAtEnd(800);
+    //cout << "\n\nArray after insert 800 at End:";
+    //cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    //MyDynamicArray.PrintItems();
+    //
+    //
+    //
+    //system("pause>0");    
+    //
+    //return 0;
+//}
--- Course 13/clsDynamicArray.h
+++ Course 13/clsDynamicArray.h
@@ -11,12 +11,13 @@ protected:
 public:
 
 	T* OriginalArray;
-	clsDynamicArray(int Size = 0) 
+	
+	clsDynamicArray(int size = 0) 
 	{
-		if (Size < 0)
-			Size = 0;
+		if (size < 0)
+			size = 0;
 
-		_Size = Size;
+		_Size = size;
 		OriginalArray = new T[_Size];
 	}
 
@@ -53,5 +54,184 @@ public:
 		cout << endl;
 	}
 
+	void Resize(int newSize) 
+	{
+		if (newSize == _Size)
+			return;
+		
+		if (newSize < 0)
+			newSize = 0;
+		
+		T* TempArray = new T[newSize];
+	
+		if (newSize < _Size)
+			_Size = newSize;
+
+		for (int i = 0; i < _Size; i++)
+		{
+			TempArray[i] = OriginalArray[i];
+		}
+
+		_Size = newSize;
+		delete[] OriginalArray;
+		OriginalArray = TempArray;
+	}
+	
+	T GetItem(int indxe) 
+	{
+		return (indxe < 0 || indxe >= _Size) ? NULL : OriginalArray[indxe];
+	}
+
+	void Clear() 
+	{
+		T* TempArray = new T[0];
+		_Size = 0;
+		delete[] OriginalArray;
+		OriginalArray = TempArray;
+	}
+
+	void Reverse() 
+	{
+		T* TempArray = new T[_Size];
+		int counte = 0;
+
+		for (int i = _Size - 1; i >= 0 ; i--)
+		{
+			TempArray[counte] = OriginalArray[i];
+			counte++;
+		}
+		
+		delete[] OriginalArray;
+		OriginalArray = TempArray;
+	}
+
+	bool DeleteItemByIndex(int index)		// This My Solution
+	{
+		if (index < 0 || index >= _Size)
+			return false;
+
+		T* TempArray = new T[_Size-1];
+
+		int newSize = 0;
+		for(int i = 0; i < _Size ; i++)
+		{
+			if (i != index) 
+			{
+				TempArray[newSize++] = OriginalArray[i];
+			}
+		}
+
+		_Size = newSize;
+		delete[] OriginalArray;
+		OriginalArray = TempArray;
+		return true;
+	}
+
+	bool DeleteItemAt(int index)
+	{
+		if (index >= _Size || index < 0)
+			return false;
+
+		_Size--;
+		T* TempArray = new T[_Size];
+
+		//copy all before index
+		for (int i = 0; i < index; i++)
+		{
+			TempArray[i] = OriginalArray[i];
+		}
+
+		//copy all after index
+		for (int i = index + 1; i < _Size + 1; i++)
+		{
+			TempArray[i - 1] = OriginalArray[i];
+		}
+
+		delete[] OriginalArray;
+		OriginalArray = TempArray;
+		return true;
+	}
+
+	bool DeleteFirstItem() 
+	{
+		return DeleteItemByIndex(0);
+	}
+
+	bool DeleteLastItem()
+	{
+		return DeleteItemByIndex(_Size - 1);
+	}
+	
+	int Find(T value) 
+	{
+		for (int  i = 0; i < _Size; i++)
+		{
+			if (OriginalArray[i] == value)
+				return i;
+		}
+		return -1;
+	}
+
+	bool DeleteItem(T value) 
+	{
+		return DeleteItemByIndex(Find(value));
+	}
+
+	bool InsertAt(int index, T value) 
+	{
+
+		if (index > _Size || index < 0)
+		{
+			return false;
+		}
+
+		_Size++;
+
+		T* TempArray = new T[_Size];
+
+		//copy all before index
+		for (int i = 0; i < index; i++)
+		{
+			TempArray[i] = OriginalArray[i];
+		}
+
+		TempArray[index] = value;
+
+		//copy all after index
+		for (int i = index; i < _Size - 1; i++)
+		{
+			TempArray[i + 1] = OriginalArray[i];
+		}
+
+		delete[] OriginalArray;
+		OriginalArray = TempArray;
+		return true;
+	}
+
+	bool InsertAtBeginning(T value) 
+	{
+		return InsertAt(0, value);
+	}
+
+	bool InsertBefore(int index, T value)
+	{
+		if (index < 1)
+			return InsertAt(0, value);
+		else
+			return InsertAt(index, value);
+	}
+
+	bool InsertAfter(int index, T value)
+	{
+		if (index >= _Size)
+			return InsertAt(_Size, value);
+		else
+			return InsertAt(index + 1, value);
+	}
+
+	bool InsertAtEnd(T value)
+	{
+		return InsertAt(_Size, value);
+	}
 };
 
